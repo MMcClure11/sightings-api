94e66f969035ad866ded86f033b51a688811371e
MMcClure11 <mcclme02@luther.edu>
2020-12-02 10:55:44 -0600

    adds create action in sightings controller

--- app/controllers/api/v1/sightings_controller.rb
+++ app/controllers/api/v1/sightings_controller.rb
@@ -5,4 +5,29 @@ class Api::V1::SightingsController < ApplicationController
     render json: sightings, status: 200
   end
 
+  def create
+    sighting = current_user.sightings.build(sighting_params)
+    byebug
+    if sighting.save 
+      render json: sighting, status: 200
+    else
+      error_resp = {
+        error: sighting.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sighting_params
+    params.require(:sighting).permit(
+      :image,
+      :identified,
+      :common_name,
+      :scientific_name,
+      :date,
+      :notes
+    )
+  end
 end
