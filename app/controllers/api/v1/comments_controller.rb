58bed0de345cdd45954ce4bc6593cf8fa61ef32d
MMcClure11 <mcclme02@luther.edu>
2020-12-07 17:58:55 -0600

    adds destroy action to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
+
 
   def index
     comments = Comment.all
@@ -10,7 +12,16 @@ class Api::V1::CommentsController < ApplicationController
     render json: comment
   end
 
+  def destroy
+    @comment.destroy
+    render json: {notice: 'Comment destroyed'}, status: 200
+  end
+
   private
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+  end
 
   def comment_params
     params.permit(:sighting_id, :content)
