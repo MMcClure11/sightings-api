10a2fea65641a0d1331733ff9cc04ab57e3933b2
MMcClure11 <mcclme02@luther.edu>
2020-12-07 13:59:38 -0600

    updates models and serializers to still show sightings under current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_many :sightings
   has_many :comments
-  has_many :sightings, through: :comments
+  # has_many :sightings, through: :comments
 
   validates :name, :username, presence: true
   validates :username, uniqueness: true
