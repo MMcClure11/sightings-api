b970e2df6588d100503004680dd36c2b41c258a9
MMcClure11 <mcclme02@luther.edu>
2020-12-07 10:07:22 -0600

    appropriately wires up SightingShowSerializer

--- app/serializers/sighting_show_serializer.rb
+++ app/serializers/sighting_show_serializer.rb
@@ -1,5 +1,5 @@
 class SightingShowSerializer < ActiveModel::Serializer
-  :id, :image, :identified, :common_name, :scientific_name, :public, :date, :time, :notes, :location, :category
+  attributes :id, :image, :identified, :common_name, :scientific_name, :public, :date, :time, :notes, :location, :category
   belongs_to :location
   belongs_to :category
 end
