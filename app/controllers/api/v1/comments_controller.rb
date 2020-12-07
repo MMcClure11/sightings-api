bb977f7a42d542d0c83deece82b6ded2cce73dc7
MMcClure11 <mcclme02@luther.edu>
2020-12-07 12:01:44 -0600

    adds create action and params for comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::CommentsController < ApplicationController
     render json: comments, status: 200
   end
 
+  def create
+    byebug
+    comment = current_user.comments.create(comment_params)
+    render json: comment
+  end
+
+  private
+
+  def comment_params
+    params.permit(:sighting_id, :content)
+  end
+
 end
