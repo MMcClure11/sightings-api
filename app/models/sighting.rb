d418000953f57220c0244985391a078d989bbab8
MMcClure11 <mcclme02@luther.edu>
2020-12-11 09:59:32 -0600

    adds dependent: :destroy to sighting for has_many :comments

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,7 +3,7 @@ class Sighting < ApplicationRecord
   belongs_to :category
   belongs_to :location
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :users, through: :comments
 
   validates :image, :date, presence: true
