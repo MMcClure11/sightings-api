515ad4fbc3c1729fd40dd3f95d2e847ff1d94cd8
MMcClure11 <mcclme02@luther.edu>
2020-12-14 10:41:56 -0600

    adds backend validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :sightings
+
+  validates :name, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
   has_many :sightings
+
+  validates :city, :region, :country, presence: true
 end
