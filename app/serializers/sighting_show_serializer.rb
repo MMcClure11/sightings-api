c5cd2b97668c5de69b535c6956d4b577a0b12250
MMcClure11 <mcclme02@luther.edu>
2020-12-23 09:53:56 -0600

    Add username to sighting show serializer

--- app/serializers/sighting_show_serializer.rb
+++ app/serializers/sighting_show_serializer.rb
@@ -1,8 +1,12 @@
 class SightingShowSerializer < ActiveModel::Serializer
-  attributes :id, :image, :identified, :common_name, :scientific_name, :public, :date, :time, :notes, :location, :category
+  attributes :id, :image, :identified, :common_name, :scientific_name, :public, :date, :time, :notes, :location, :category, :username
   belongs_to :location
   belongs_to :category
   has_many :comments
+
+  def username
+    object.user.username
+  end
 
   def comments
     object.comments.order("created_at ASC")
